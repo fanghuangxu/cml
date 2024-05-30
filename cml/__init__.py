4af1a79672542bc5b1275d9ba5b11917bba2fb49
fanghuangxu <159121972+fanghuangxu@users.noreply.github.com>
2024-05-30 21:27:48 +0800

    Update __init__.py

--- cml/__init__.py
+++ cml/__init__.py
@@ -1,2 +1,8 @@
 from cml import command,forge,mod,server,utils,java
 __all__=['command','forge','mod','server','utils','java']
+print('hello i\'m china-minecraft-launcher libany')
+import datetime
+
+# 获取当前年份
+current_year = datetime.datetime.now().year
+print(f"Minecraft is {int(current_year)-2011} years old.")
